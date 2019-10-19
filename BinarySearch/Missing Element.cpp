57f5dfffa1a28856938f141135b89289fe8450b3
Nishant <nishantsaini97286@gmail.com>
2019-10-19 10:34:59 +0530

    missing element in a sorted array

--- BinarySearch/Missing Element.cpp
+++ BinarySearch/Missing Element.cpp
@@ -6,31 +6,15 @@ int missing(int ar[], int N)
     while (l <= r) {
 
         int mid = (l + r) / 2;
-
-        // If this is the first element
-        // which is not index + 1, then
-        // missing element is mid+1
-        if (ar[mid] != mid + 1 &&
-                        ar[mid - 1] == mid)
+        if (ar[mid] != mid + 1 && ar[mid - 1] == mid)
             return mid + 1;
-
-        // if this is not the first missing
-        // element search in left side
         if (ar[mid] != mid + 1)
             r = mid - 1;
-
-        // if it follows index+1 property then
-        // search in right side
         else
             l = mid + 1;
     }
-
-    // if no element is missing
     return -1;
 }
-
-
-
 int main()
 {
   int n;
