ba3d0584957be6e8a2454bd8f7eb1a9148f31c18
Nishant <nishantsaini97286@gmail.com>
2019-11-20 19:02:51 +0530

    find spanning tree using kruskals algorithm

---
+++ graph/mst.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+class Edge{
+public:
+int source;
+int dest;
+int weight;
+};
+bool compare(Edge e1,Edge e2){
+return e1.weight<e2.weight;
+}
+
+int findparent(int v,int *parent)
+{
+  if(parent[v]==v)
+    return v;
+  return findparent(parent[v],parent);
+}
+
+void kruskals(Edge *input,int n,int e){
+Edge *output=new Edge[n-1];
+sort(input,input+e,compare);
+int *parent=new int[n];
+for(int i=0;i<n;i++)
+    parent[i]=i;
+int count=0;
+int i=0;
+while(count!=n-1)
+{
+  int parentsource=findparent(input[i].source,parent);
+  int parentdest=findparent(input[i].dest,parent);
+  if(parentsource!=parentdest)
+  {
+    output[count]=input[i];
+    count++;
+    parent[parentsource]=parentdest;
+  }
+  i++;
+}
+for(int i=0;i<n-1;i++)
+{
+    if(output[i].source<output[i].dest)
+        cout<<output[i].source<<" "<<output[i].dest<<" "<<output[i].weight<<endl;
+    else
+        cout<<output[i].dest<<" "<<output[i].source<<" "<<output[i].weight<<endl;
+}
+}
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    Edge *edge=new Edge[e];
+    for(int i=0;i<e;i++)
+    {
+        int s,d,w;
+        cin>>s>>d>>w;
+        edge[i].source=s;
+        edge[i].dest=d;
+        edge[i].weight=w;
+    }
+    kruskals(edge,n,e);
+}
